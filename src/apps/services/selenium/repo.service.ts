e2ced05525cd48a8f90e2875c13aafbdfcefc041
LeVanHieu0509 <levanhieu.huflit@gmail.com>
2024-08-12 23:09:59 +0700

    Revert "phuc"
    This reverts commit 7eaf37e793c34b734bd0b48c85f72187c2b2831b.

--- src/apps/services/selenium/repo.service.ts
+++ src/apps/services/selenium/repo.service.ts
@@ -67,7 +67,7 @@ export const postToGroup = async (driver: WebDriver, content: string, files: str
 
     for (const file of files) {
       let f = cloneDeep(file) as any;
-      const imagePath = path.resolve(__dirname, `C:/Users/Admin/Downloads/${f.fileName}`);
+      const imagePath = path.resolve(__dirname, `D:/image/${f.fileName}`);
       await uploadImage(driver, imagePath);
     }
 
